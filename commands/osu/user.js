5aa5cbadd18c3959602f14896556e8c04bf336f5
khanhmuy <maikhanhhuy0608@outlook.com>
2024-02-29 12:41:20 +0700

    idk what im doing

--- commands/osu/user.js
+++ commands/osu/user.js
@@ -1,5 +1,6 @@
 const {EmbedBuilder, SlashCommandBuilder} = require('discord.js');
 const {v2} = require('osu-api-extended');
+const Vibrant = require('node-vibrant');
 module.exports = {
     guildOnly: true,
     data: new SlashCommandBuilder()
@@ -15,11 +16,14 @@ module.exports = {
             const osuId = interaction.client.data.get(`user.${interaction.user.id}.osuId`);
             if (osuId === null) return interaction.editReply({content: 'You must provide a user.', ephemeral: true});
             const res = await v2.user.details(osuId);
+            let color = null
+            color = await Vibrant.from(res.avatar_url).getPalette()
+            color = color.Vibrant.hex
             const embed = new EmbedBuilder()
-                .setTitle(`${res.username}'s profile for osu!std`)
+                .setTitle(`${res.username}'s profile for osu!standard`)
                 .setURL(`https://osu.ppy.sh/users/${res.id}`)
                 .setThumbnail(`${res.avatar_url}`)
-                .setColor('#f5c2e7')
+                .setColor(color)
                 .addFields(
                     {name: 'Rank', value: `#${res.statistics.global_rank} (:flag_${res.country_code.toLowerCase()}:#${res.statistics.country_rank})`, inline: true},
                     {name: 'Peak Rank', value: `#${res.rank_highest.rank}`, inline: true},
@@ -28,7 +32,7 @@ module.exports = {
                     {name: 'Accuracy', value: `${res.statistics.hit_accuracy.toFixed(2)}%`, inline: true},
                     {name: 'Total Score', value: `${res.statistics.total_score}`, inline: true},
                     {name: 'Ranked Score', value: `${res.statistics.ranked_score}`, inline: true},
-                    {name: 'Play Count', value: `${res.statistics.play_count}`, inline: true},
+                    {name: 'Play Count', value: `${res.statistics.play_count} (${Math.round(res.statistics.play_time/3600)} hrs)`, inline: true},
                     {name: 'Ranks', value: `X: ${res.statistics.grade_counts.ss} | XH: ${res.statistics.grade_counts.ssh}\nS: ${res.statistics.grade_counts.s} | SH: ${res.statistics.grade_counts.sh}\nA: ${res.statistics.grade_counts.a}`, inline: true},
                 )
             await interaction.editReply({embeds: [embed]});
@@ -38,11 +42,14 @@ module.exports = {
                 await interaction.editReply({content: 'User not found.', ephemeral: true});
             } else if(search.user.data[0].username) {
                 const res = await v2.user.details(search.user.data[0].id);
+                let color = null
+                color = await Vibrant.from(res.avatar_url).getPalette()
+                color = color.Vibrant.hex
                 const embed = new EmbedBuilder()
-                    .setTitle(`${res.username}'s profile for osu!std`)
+                    .setTitle(`${res.username}'s profile for osu!standard`)
                     .setURL(`https://osu.ppy.sh/users/${res.id}`)
                     .setThumbnail(`${res.avatar_url}`)
-                    .setColor('#f5c2e7')
+                    .setColor(color)
                     .addFields(
                         {name: 'Rank', value: `#${res.statistics.global_rank} ( :flag_${res.country_code.toLowerCase()}:#${res.statistics.country_rank} )`, inline: true},
                         {name: 'Peak Rank', value: `#${res.rank_highest.rank}`, inline: true},
@@ -51,7 +58,7 @@ module.exports = {
                         {name: 'Accuracy', value: `${res.statistics.hit_accuracy.toFixed(2)}%`, inline: true},
                         {name: 'Total Score', value: `${res.statistics.total_score}`, inline: true},
                         {name: 'Ranked Score', value: `${res.statistics.ranked_score}`, inline: true},
-                        {name: 'Play Count', value: `${res.statistics.play_count}`, inline: true},
+                        {name: 'Play Count', value: `${res.statistics.play_count} (${Math.round(res.statistics.play_time/3600)} hrs)`, inline: true},
                         {name: 'Ranks', value: `X: ${res.statistics.grade_counts.ss} | XH: ${res.statistics.grade_counts.ssh}\nS: ${res.statistics.grade_counts.s} | SH: ${res.statistics.grade_counts.sh}\nA: ${res.statistics.grade_counts.a}`, inline: true},
                     )
                 await interaction.editReply({embeds: [embed]});
